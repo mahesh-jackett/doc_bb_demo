f081549633171e5bce15197cddfc204b8360b625
Mahesh Deshwal <m.deshwal93@gmail.com>
2022-10-14 13:46:51 +0530

    added support for EigenGrad cam,, updated grad-cam in requirements and changed code in demo + helpers

--- demo.py
+++ demo.py
@@ -1,38 +1,44 @@
-from helpers import build_model, get_results
-
-import cv2
+from helpers import *
 import streamlit as st
-from PIL import Image
-import numpy as np
-from io import BytesIO
 
 
-def process_image(input_image):
-    img = Image.fromarray(input_image)
-    buffer = BytesIO()
-    img.save(buffer, format="jpeg")
-    return buffer.getvalue()
 
-st.set_page_config(page_title="Bounding Box",page_icon="☯️")
+st.set_page_config(page_title="BB Model",page_icon="☯️", layout="wide")
 
 if 'THRESH' not in st.session_state: # Since model is always dependent on threshold
     st.session_state['THRESH'] = 0.6
-    st.session_state['model'] = build_model(st.session_state['THRESH'])
+    st.session_state['model'] = TorchModel(st.session_state['THRESH'])
 
 if 'uploaded' not in st.session_state:
     st.session_state['uploaded'] = None
 
-if 'result' not in st.session_state: # download can only exists iff there's result
-    st.session_state['result'] = None
-    st.session_state['download'] = None
+if 'base_result' not in st.session_state:
+    st.session_state['base_result'] = None
+    st.session_state['eigen'] = None
+    st.session_state['norm_eigen'] = None
+    st.session_state['meta'] = None
 
   
 def slider_callback():
-    st.session_state['model'] = build_model(st.session_state['THRESH'])
+    ''''
+    Build the modle again and clear the sesssions to get new predictions on new model
+    '''
+    with st.spinner("Building Model again with new threshold..."):
+        st.session_state['model'] = TorchModel(st.session_state['THRESH'])
+        upload_callback() # it'll force the predictions to be run again
+
+
+def upload_callback():
+    '''
+    Clear the session states so that we can do predictions on the freshly uploaded image
+    '''
+    st.session_state['base_result'] = None
+    st.session_state['eigen'] = None
+    st.session_state['norm_eigen'] = None
 
 
 with st.sidebar:
-    st.markdown("""## Change Confidence Threshold""")
+    st.markdown("""## Change Confidence Threshold to see variations in results""")
     THRESH = st.slider('', key = 'THRESH',
     min_value = 0.1, max_value = 0.99, step = 0.05,
     help = "Enter a number between 0.1-.99: Detections are filtered on this confidence",
@@ -40,30 +46,58 @@ with st.sidebar:
         
 
 st.markdown("## Upload Image")
-
-st.session_state['uploaded'] = st.file_uploader("", type = ["jpg", "png", "jpeg"], key = "upload", help = "Upload the Image from your device")
+st.session_state['uploaded'] = st.file_uploader("", type = ["jpg", "png", "jpeg"], on_change = upload_callback, help = "Upload the Image from your device")
 
 if st.session_state['uploaded'] is not None:
     try:
         file_bytes = np.asarray(bytearray(st.session_state['uploaded'].read()), dtype=np.uint8) # open with OpenCv
         st.session_state['uploaded'] = cv2.imdecode(file_bytes, 1) # change session state 
+    
     except Exception as e:
-        st.markdown(f"""<b><span style="color: #ff0000">Image Loading Error: </span>{e}</b><br>Take a screenshot and reload""", unsafe_allow_html=True)
+        st.exception(f"""<b><span style="color: #ff0000">Image Loading Error: </span>{e}</b><br>""", unsafe_allow_html=True)
 
     try:
-        with st.spinner("Processing Image..."):
-            st.session_state['result'] = get_results(st.session_state['model'], st.session_state['uploaded']) # Get Bounding Boxes
-    
-        st.markdown(f"""<b> Current Threshold: <span style="color: #ff0000">{str(st.session_state['THRESH'])} </span> </b>|| Change threshold to see variations in results""",
-        unsafe_allow_html=True) # Show current threshold value
+        if st.session_state['base_result'] is None:
+            with st.spinner("Getting Model Inference..."):
+                float_image, tensor_image, boxes, labels, classes, COLORS = get_results(st.session_state['model'], st.session_state['uploaded'])# Get Bounding Boxes
+                st.session_state['base_result'] = draw_boxes(st.session_state['uploaded'],boxes, labels, classes, COLORS)
+                st.session_state['meta'] = float_image, tensor_image, boxes, labels, classes, COLORS
+                st.session_state['show'] = st.session_state['base_result']
+        else:
+            float_image, tensor_image, boxes, labels, classes, COLORS = st.session_state['meta']
 
-        try:
-            st.session_state['download'] = process_image(st.session_state['result'])
-            _ = st.download_button(label="Download",data = st.session_state['download'], file_name="image.jpeg",mime="image/jpeg")
-        except Exception as e:
-            st.markdown(f"""<b><span style="color: #ff0000">Download Error: </span>{e}</b><br>""", unsafe_allow_html=True)
-
-        st.image(st.session_state['result'], channels="RGB")
-    
     except Exception as e:
-        st.markdown(f"""<b><span style="color: #ff0000">Inference Error: </span>{e}</b><br>Take a screenshot and reload""", unsafe_allow_html=True)
+        st.exception(f"""<b><span style="color: #ff0000">Inference Error: </span>{e}</b><br>""", unsafe_allow_html=True)
+    
+
+    st.markdown("<h5>Choose the type of results you want to see. Save the image results by clicking 'Save image as..'</h5><br>", unsafe_allow_html = True)
+    
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        if st.button("Default Predictions"): 
+            st.session_state['show'] = st.session_state['base_result']
+
+
+    with col2:
+        if st.button("EigenCam: Box Only"):
+            if st.session_state['norm_eigen'] is None:
+                with st.spinner("Getting Box Normalized Eigen Cam Results..."):
+                    st.session_state['eigen'], st.session_state['norm_eigen'] = eigen_res(st.session_state['model'], float_image, tensor_image, labels, boxes, classes, COLORS)
+            
+            st.session_state['show'] = st.session_state['norm_eigen']
+    
+
+    with col3:
+        if st.button("EigenCam: Whole Image",):
+            if st.session_state['eigen'] is None:
+                with st.spinner("Getting Eigen Cam Results..."):
+                    st.session_state['eigen'], st.session_state['norm_eigen'] = eigen_res(st.session_state['model'], float_image, tensor_image, labels, boxes, classes, COLORS)
+            
+            st.session_state['show'] = st.session_state['eigen']
+    
+    
+    try:
+        st.image(st.session_state['show'], channels="RGB")
+    except Exception as e:
+        st.exception(f"Image Rendering Error: {e}")
--- helpers.py
+++ helpers.py
@@ -1,39 +1,28 @@
 # !python -m pip install 'git+https://github.com/facebookresearch/detectron2.git' --quiet
 
-
-import numpy as np
-import gdown
-import os
-import random
-import torch
-
-import warnings
-warnings.filterwarnings('ignore') #Ignore "future" warnings and Data-Frame-Slicing warnings.
-
 try:
     from detectron2.config import get_cfg  
+
 except ModuleNotFoundError:
     import subprocess
     import sys
     subprocess.check_call([sys.executable, "-m", "pip", "install", 'git+https://github.com/facebookresearch/detectron2.git'])
 
-# detectron2
-from detectron2.config import get_cfg
-from detectron2.engine import DefaultPredictor
-from detectron2.utils.visualizer import Visualizer
+finally:
+    import numpy as np
+    import cv2, torch, os, gdown
 
-# -------------------------------
+    from pytorch_grad_cam import EigenCAM
+    from pytorch_grad_cam.utils.model_targets import FasterRCNNBoxScoreTarget
+    from pytorch_grad_cam.utils.image import show_cam_on_image, scale_cam_image
 
-seed = 42
-os.environ["PL_GLOBAL_SEED"] = str(seed)
-random.seed(seed)
-np.random.seed(seed)
-torch.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
+    from detectron2.modeling import build_model
+    from detectron2.checkpoint import DetectionCheckpointer
+    from detectron2.config import get_cfg
 
-# ----------------------------------
 
-def build_model(THRESH:float = 0.6):
+
+def build_cfg(THRESH:float = 0.6):
 
     cfg = get_cfg()
 
@@ -41,7 +30,7 @@ def build_model(THRESH:float = 0.6):
     cfg.merge_from_file(config_name)
 
 
-    if not torch.cuda.is_available(): cfg.MODEL.DEVICE = "cpu"
+    cfg.MODEL.DEVICE = "cpu"
 
     cfg.DATALOADER.NUM_WORKERS: 2
     cfg.TEST.EVAL_PERIOD = 20 # Evaluate after N epochs
@@ -65,16 +54,126 @@ def build_model(THRESH:float = 0.6):
     cfg.SOLVER.GAMMA = 0.05
     cfg.SOLVER.CHECKPOINT_PERIOD = 20  # Save weights after these many epochs
 
-
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = THRESH
-    return DefaultPredictor(cfg)
+    return cfg
 
 
-def get_results(predictor, image):
-    output = predictor(image)
-    v = Visualizer(image[:,:,::-1]) # BGR to RGB format : https://github.com/youngwanLEE/vovnet-detectron2/issues/16
-    v = v.draw_instance_predictions(output['instances'].to('cpu'))
-    return v.get_image()
+class TorchModel(torch.nn.Module):
+    def __init__(self, thresh:float = 0.6) -> None:
+        super().__init__()
+        cfg = build_cfg(thresh) # Own function in helpers.py to load weights
+        self.model = build_model(cfg) # Build Model
+        _ = DetectionCheckpointer(self.model).load(cfg.MODEL.WEIGHTS)  # Load weights
+        self.model = self.model.eval() # In evaluation mode
+    
+    def forward(self, INPUT):
+        if isinstance(INPUT, (np.ndarray, torch.Tensor)):
+            INPUT = [{"image":INPUT}]
+
+        with torch.no_grad():
+            outputs = self.model(INPUT)[0]['instances']
+        
+        boxes, labels, scores = outputs.pred_boxes.tensor, outputs.pred_classes, outputs.scores.detach()
+
+        return boxes, labels, scores
+
+
+def draw_boxes( image, boxes, labels, classes, COLORS):
+    '''
+    Accept BGR image and plot Bounding boxe, labels on top of it
+    '''
+    for i, box in enumerate(boxes):
+        color = COLORS[labels[i]]
+        cv2.rectangle(
+            image,
+            (int(box[0]), int(box[1])),
+            (int(box[2]), int(box[3])),
+            color, 2)
+
+        cv2.putText(image, classes[i], (int(box[0]), int(box[1] - 5)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, color, 2,
+                    lineType=cv2.LINE_AA)
+
+    return image[:,:,::-1] # Return BGR Image
+
+
+def renormalize_cam_in_bounding_boxes(image_float_np, grayscale_cam, boxes, classes, labels, colors):
+    """Normalize the CAM to be in the range [0, 1] 
+    inside every bounding boxes, and zero outside of the bounding boxes. """
+    renormalized_cam = np.zeros(grayscale_cam.shape, dtype=np.float32)
+    images = []
+    for x1, y1, x2, y2 in boxes:
+        img = renormalized_cam * 0
+        img[y1:y2, x1:x2] = scale_cam_image(grayscale_cam[y1:y2, x1:x2].copy())    
+        images.append(img)
+    
+    renormalized_cam = np.max(np.float32(images), axis = 0)
+    renormalized_cam = scale_cam_image(renormalized_cam)
+    eigencam_image_renormalized = show_cam_on_image(image_float_np, renormalized_cam)
+    image_with_bounding_boxes = draw_boxes(eigencam_image_renormalized, boxes, labels, classes, colors)
+    return image_with_bounding_boxes
+
+
+def fasterrcnn_reshape_transform(x):
+    '''
+    Override the code from the original repo due to the same issue as: 
+    Solution for the Issue: https://github.com/jacobgil/pytorch-grad-cam/issues/278
+    Pass in your Pooling Layers name: Last one in my model is "p6"
+    '''
+    # for key, value in x.items():print(key)
+    layer_name = "p6"
+
+    target_size = x[layer_name].size()[-2:]
+    activations = []
+    for key, value in x.items():
+        activations.append(
+            torch.nn.functional.interpolate(
+                torch.abs(value),
+                target_size,
+                mode='bilinear'))
+    activations = torch.cat(activations, axis=1)
+    return activations
+
+
+def get_results(model, image):
+    '''
+    Get results from the model given an image
+    '''
+    float_image = np.float32(image/255.) #  Normalized BGR image between 0-1
+
+    tensor_image = torch.from_numpy(image.copy()).permute(2, 0, 1) # [B, channels, W, H]
+    tensor_boxes, labels, scores = model(tensor_image)
+
+    boxes = np.int32(tensor_boxes.numpy())
+    labels = [i for i in range(len(labels))]
+    classes = [f"Q: {str(i)}" for i in labels]
+
+    COLORS = np.random.uniform(0, 255, size=(len(set(labels)), 3)) # for plotting purpose
+
+    return float_image, tensor_image, boxes, labels, classes, COLORS
+
+
+def eigen_res(model, float_image, tensor_image, labels, boxes, classes, colors):
+    '''
+    Run the EigenCam to get results
+    '''
+    targets = [FasterRCNNBoxScoreTarget(labels=labels, bounding_boxes=boxes)] # this not executed in EigenCam just init
+    target_layers = [model.model.backbone]
+
+    cam = EigenCAM(model,target_layers, use_cuda=torch.cuda.is_available(), reshape_transform=fasterrcnn_reshape_transform)
+
+    grayscale_cam = cam(tensor_image, targets=targets, eigen_smooth=True)
+    grayscale_cam = grayscale_cam[0, :] # Take the first image in the batch
+
+    cam_image = show_cam_on_image(float_image, grayscale_cam) 
+
+    RGB_unnormalized_image = draw_boxes(cam_image, boxes, labels, classes, colors) # without normalizing
+    RGB_normalized_image = renormalize_cam_in_bounding_boxes(float_image, grayscale_cam,  boxes, classes, labels, colors) # with Normalized Boxes
+
+    return RGB_unnormalized_image, RGB_normalized_image
+
+
+
 
 
 
